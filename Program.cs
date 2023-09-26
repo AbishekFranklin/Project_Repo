346e0267414e7ae9ddeda7d2c7f52d1eac145930
AbishekFranklin <68886484+AbishekFranklin@users.noreply.github.com>
2023-09-26 19:46:57 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+global using OnlineShopping.Data;
+using OnlineShopping.Models;
+using OnlineShopping.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IShoppingService, ShoppingService>();
+builder.Services.AddScoped<CartCalculator>();
+builder.Services.AddScoped<CardValidation>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
